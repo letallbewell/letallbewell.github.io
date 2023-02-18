0a58c838220e176d23eec3105f2b70280fe83d4c
Mathew Alex <43025445+letallbewell@users.noreply.github.com>
2023-02-18 14:05:51 +0530

    add notes

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,4 @@
-function openCity(evt, cityName) {
+function open_tab(evt, cityName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -12,17 +12,17 @@ function openCity(evt, cityName) {
     evt.currentTarget.className += " active";
   }
   
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
 
-  function change_link_targets() {
+function change_link_targets() {
 
-    var links = document.links;
-    for (var i = 0; i < links.length; i++) {
-        if (!(links[i].href.includes('github.io'))) {
-            links[i].target = "_blank";
-        }
-    }
-    document.links = links;
+  var links = document.links;
+  for (var i = 0; i < links.length; i++) {
+      if (!(links[i].href.includes('github.io'))) {
+          links[i].target = "_blank";
+      }
   }
+  document.links = links;
+}
 
